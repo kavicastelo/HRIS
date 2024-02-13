107eef4b8f2d992fc9b9593fdb9e3be3480f84ef
kavi castelo <kavindu123450@gmail.com>
2024-02-13 12:32:33 +0530

    transfer letters
    create basic REST APIs to handle transfer letter requests

---
+++ src/main/java/com/hris/HRIS/controller/TransferController.java
@@ -0,0 +1,140 @@
+package com.hris.HRIS.controller;
+
+import com.hris.HRIS.dto.ApiResponse;
+import com.hris.HRIS.model.TransferModel;
+import com.hris.HRIS.repository.TransferRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/v1/transfer")
+public class TransferController {
+
+    @Autowired
+    TransferRepository transferRepository;
+
+    @PostMapping("/save")
+    public ResponseEntity<ApiResponse> saveLetter(@RequestBody TransferModel transferModel){
+        transferRepository.save(transferModel);
+
+        ApiResponse apiResponse = new ApiResponse("Transfer Letter Requested successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @GetMapping("/get/all")
+    public List<TransferModel> getAllLetters(){
+        return transferRepository.findAll();
+    }
+
+    @GetMapping("/get/id/{id}")
+    public ResponseEntity<TransferModel> getLetterById(@PathVariable String id){
+        Optional<TransferModel> transferModelOptional = transferRepository.findById(id);
+
+        return transferModelOptional.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/get/email/{email}")
+    public ResponseEntity<TransferModel> getLetterByEmail(@PathVariable String email){
+        Optional<TransferModel> transferModelOptional = transferRepository.findByEmail(email);
+
+        return transferModelOptional.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity<ApiResponse> deleteLetter(@PathVariable String id){
+        transferRepository.deleteById(id);
+
+        ApiResponse apiResponse = new ApiResponse("Request deleted successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @DeleteMapping("/delete/email/{email}")
+    public ResponseEntity<ApiResponse> deleteLetterByEmail(@PathVariable String email){
+        transferRepository.deleteByEmail(email);
+
+        ApiResponse apiResponse = new ApiResponse("Request deleted successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PutMapping("/update/id/{id}")
+    public ResponseEntity<ApiResponse> updateLetter(@PathVariable String id, @RequestBody TransferModel transferModel){
+        Optional<TransferModel> transferModelOptional = transferRepository.findById(id);
+
+        if(transferModelOptional.isPresent()){
+            TransferModel existingLetter = transferModelOptional.get();
+            existingLetter.setName(transferModel.getName());
+            existingLetter.setEmail(transferModel.getEmail());
+            existingLetter.setPhone(transferModel.getPhone());
+            existingLetter.setAddress(transferModel.getAddress());
+            existingLetter.setJobData(transferModel.getJobData());
+            existingLetter.setDate(transferModel.getDate());
+            existingLetter.setDoj(transferModel.getDoj());
+            existingLetter.setPhoto(transferModel.getPhoto());
+            existingLetter.setReason(transferModel.getReason());
+            existingLetter.setApproved(transferModel.getApproved());
+
+            transferRepository.save(existingLetter);
+        }
+
+        ApiResponse apiResponse = new ApiResponse("Employee updated successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PutMapping("/update/email/{email}")
+    public ResponseEntity<ApiResponse> updateLetterByEmail(@PathVariable String email, @RequestBody TransferModel transferModel){
+        Optional<TransferModel> transferModelOptional = transferRepository.findByEmail(email);
+
+        if(transferModelOptional.isPresent()){
+            TransferModel existingLetter = transferModelOptional.get();
+            existingLetter.setName(transferModel.getName());
+            existingLetter.setEmail(transferModel.getEmail());
+            existingLetter.setPhone(transferModel.getPhone());
+            existingLetter.setAddress(transferModel.getAddress());
+            existingLetter.setJobData(transferModel.getJobData());
+            existingLetter.setDate(transferModel.getDate());
+            existingLetter.setDoj(transferModel.getDoj());
+            existingLetter.setPhoto(transferModel.getPhoto());
+            existingLetter.setReason(transferModel.getReason());
+            existingLetter.setApproved(transferModel.getApproved());
+
+            transferRepository.save(existingLetter);
+        }
+
+        ApiResponse apiResponse = new ApiResponse("Employee updated successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PutMapping("/approve/id/{id}")
+    public ResponseEntity<ApiResponse> approveLetter(@PathVariable String id){
+        Optional<TransferModel> transferModelOptional = transferRepository.findById(id);
+
+        if(transferModelOptional.isPresent()){
+            TransferModel existingLetter = transferModelOptional.get();
+            existingLetter.setApproved(true);
+
+            transferRepository.save(existingLetter);
+        }
+
+        ApiResponse apiResponse = new ApiResponse("Employee approved successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+
+    @PutMapping("/approve/email/{email}")
+    public ResponseEntity<ApiResponse> approveLetterByEmail(@PathVariable String email){
+        Optional<TransferModel> transferModelOptional = transferRepository.findByEmail(email);
+
+        if(transferModelOptional.isPresent()){
+            TransferModel existingLetter = transferModelOptional.get();
+            existingLetter.setApproved(true);
+
+            transferRepository.save(existingLetter);
+        }
+
+        ApiResponse apiResponse = new ApiResponse("Employee approved successfully");
+        return ResponseEntity.ok(apiResponse);
+    }
+}
