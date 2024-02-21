0a6daccf1f81f5bd12198d933e4dcb6114a141b4
kavi castelo <kavindu123450@gmail.com>
2024-02-21 16:21:14 +0530

    Update MultimediaController.java

--- src/main/java/com/hris/HRIS/controller/MultimediaController.java
+++ src/main/java/com/hris/HRIS/controller/MultimediaController.java
@@ -4,10 +4,7 @@ import com.hris.HRIS.model.MultimediaModel;
 import com.hris.HRIS.repository.MultimediaRepository;
 import com.hris.HRIS.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -37,14 +34,23 @@ public class MultimediaController {
     }
 
     @GetMapping("/get/{id}")
-    public ResponseEntity<byte[]> getPhoto(@PathVariable String id) {
-        byte[] photoContent = photoService.getMultimediaContent(id);
+    public ResponseEntity<byte[]> getMultimedia(@PathVariable String id) {
+        MultimediaModel multimedia = photoService.getMultimedia(id);
 
-        if (photoContent != null) {
+        if (multimedia != null && multimedia.getFile() != null && multimedia.getFile().getData() != null) {
+            byte[] content = multimedia.getFile().getData();
+
             HttpHeaders headers = new HttpHeaders();
-            headers.setContentType(MediaType.IMAGE_JPEG); // Adjust content type based on your file type
 
-            return new ResponseEntity<>(photoContent, headers, HttpStatus.OK);
+            // Set content type dynamically based on multimedia content type
+            if (multimedia.getContentType() != null) {
+                headers.setContentType(MediaType.parseMediaType(multimedia.getContentType()));
+            }
+
+            // Set Content-Disposition header to force download or display inline
+            headers.setContentDisposition(ContentDisposition.builder("attachment").filename("filename").build());
+
+            return new ResponseEntity<>(content, headers, HttpStatus.OK);
         } else {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
