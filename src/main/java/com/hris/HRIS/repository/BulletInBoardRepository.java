00e33be57995d154c3089e1a4ccf9657ab86434b
kavi castelo <kavindu123450@gmail.com>
2024-02-26 14:08:31 +0530

    Update BulletInBoardRepository.java
    [update:] Remove unnecessary repository methods

--- src/main/java/com/hris/HRIS/repository/BulletInBoardRepository.java
+++ src/main/java/com/hris/HRIS/repository/BulletInBoardRepository.java
@@ -7,7 +7,4 @@ import java.util.List;
 import java.util.Optional;
 
 public interface BulletInBoardRepository extends MongoRepository<BulletInBoardModel, String> {
-    Optional<List<BulletInBoardModel>> findAllByMessages(String id);
-
-    Optional<List<BulletInBoardModel>> findAllByDocuments(String id);
 }
