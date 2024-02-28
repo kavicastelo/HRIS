d8f6c79d92e6cd603627e7ab7f9a05b02ce662ac
kavi castelo <kavindu123450@gmail.com>
2024-02-28 17:44:09 +0530

    news feed wrapper

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./components/home/home.component";
 import {NotFoundComponent} from "./shared/components/not-found/not-found.component";
 import {ProfileComponent} from "./shared/components/profile/profile.component";
+import {FeedWrapperComponent} from "./components/feed-wrapper/feed-wrapper.component";
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'feed', component: FeedWrapperComponent },
   { path: 'profile', component: ProfileComponent },
   { path: '**', component:NotFoundComponent }
 ];
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -14,6 +14,9 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {FormsModule} from "@angular/forms";
+import { FeedWrapperComponent } from './components/feed-wrapper/feed-wrapper.component';
+import {MatIconModule} from "@angular/material/icon";
+import {MatSidenavModule} from "@angular/material/sidenav";
 
 @NgModule({
   declarations: [
@@ -22,7 +25,8 @@ import {FormsModule} from "@angular/forms";
     NotFoundComponent,
     ProfileComponent,
     PostImageComponent,
-    PostTextComponent
+    PostTextComponent,
+    FeedWrapperComponent
   ],
   imports: [
     BrowserModule,
@@ -32,7 +36,9 @@ import {FormsModule} from "@angular/forms";
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    MatIconModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
