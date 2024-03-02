f13dea724258a40de5ac51e3d19cf16837cf31a4
kavi castelo <kavindu123450@gmail.com>
2024-03-03 00:29:01 +0530

    add loggers
    add loggers to both frontend and backend

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,27 +1,28 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
-import { NotFoundComponent } from './shared/components/not-found/not-found.component';
-import { ProfileComponent } from './shared/components/profile/profile.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { PostImageComponent } from './shared/components/post-image/post-image.component';
-import { PostTextComponent } from './shared/components/post-text/post-text.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {HomeComponent} from './components/home/home.component';
+import {NotFoundComponent} from './shared/components/not-found/not-found.component';
+import {ProfileComponent} from './shared/components/profile/profile.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {PostImageComponent} from './shared/components/post-image/post-image.component';
+import {PostTextComponent} from './shared/components/post-text/post-text.component';
 import {MatDialogModule} from "@angular/material/dialog";
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {FormsModule} from "@angular/forms";
-import { FeedWrapperComponent } from './components/feed-wrapper/feed-wrapper.component';
+import {FeedWrapperComponent} from './components/feed-wrapper/feed-wrapper.component';
 import {MatIconModule} from "@angular/material/icon";
 import {MatSidenavModule} from "@angular/material/sidenav";
-import { FeedComponent } from './shared/components/feed/feed.component';
-import { ChatListComponent } from './shared/components/chat-list/chat-list.component';
-import { AnnouncementAreaComponent } from './shared/components/announcement-area/announcement-area.component';
+import {FeedComponent} from './shared/components/feed/feed.component';
+import {ChatListComponent} from './shared/components/chat-list/chat-list.component';
+import {AnnouncementAreaComponent} from './shared/components/announcement-area/announcement-area.component';
 import {MatExpansionModule} from "@angular/material/expansion";
-import { LogInComponent } from './components/login/login.component';
+import {LogInComponent} from './components/login/login.component';
+import {LoggerModule, NgxLoggerLevel} from "ngx-logger";
 
 @NgModule({
   declarations: [
@@ -37,21 +38,22 @@ import { LogInComponent } from './components/login/login.component';
     AnnouncementAreaComponent,
     LogInComponent
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        BrowserAnimationsModule,
-        MatDialogModule,
-        MatFormFieldModule,
-        MatInputModule,
-        MatButtonModule,
-        FormsModule,
-        MatIconModule,
-        MatSidenavModule,
-        MatExpansionModule
-
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatExpansionModule,
+    LoggerModule.forRoot({level: NgxLoggerLevel.DEBUG})
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- client/src/app/components/home/home.component.ts
+++ client/src/app/components/home/home.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NGXLogger} from "ngx-logger";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+  constructor(private logger: NGXLogger) {
+    this.logger.debug('HomeComponent initialized');
+    // this.logger.info('This is an info message');
+    // this.logger.error('This is an error message');
+  }
+  ngOnInit(): void {
+  }
 
 }
