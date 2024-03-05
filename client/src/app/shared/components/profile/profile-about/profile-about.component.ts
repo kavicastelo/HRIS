39b95467b7813e5409d258a93f4d5578199ef8d9
kavi castelo <kavindu123450@gmail.com>
2024-03-05 22:35:08 +0530

    [sub]: profile-about

--- client/src/app/shared/components/profile/profile-about/profile-about.component.ts
+++ client/src/app/shared/components/profile/profile-about/profile-about.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {employeeDataStore} from "../../../data-stores/employee-data-store";
+import {ActivatedRoute, Router} from "@angular/router";
+import {MatDialog} from "@angular/material/dialog";
+import {ThemeService} from "../../../../services/theme.service";
+import {NGXLogger} from "ngx-logger";
 
 @Component({
   selector: 'app-profile-about',
   templateUrl: './profile-about.component.html',
   styleUrls: ['./profile-about.component.scss']
 })
-export class ProfileAboutComponent {
+export class ProfileAboutComponent implements OnInit {
+  employeeDataStore = employeeDataStore
+  employee: any
+  userId: any;
 
+  constructor(private themeService: ThemeService, private dialog: MatDialog, private router: Router, private route: ActivatedRoute, private logger: NGXLogger) {
+  }
+  ngOnInit(): void {
+    this.getUser();
+  }
+
+  getUser() {
+    employeeDataStore.forEach((emp) => {
+      this.route.paramMap.subscribe(params => {
+        this.userId = params.get('id');
+
+        if (emp.id == this.userId) {
+          this.employee = [emp];
+          this.logger.info(emp)
+        }
+      })
+    })
+  }
 }
