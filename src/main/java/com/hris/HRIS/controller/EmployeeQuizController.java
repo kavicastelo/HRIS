566ecc51072589018f4d7dbdb82c5ec4d59f6cfa
Himashana <himashanasuraweera5@gmail.com>
2024-03-16 20:29:00 +0530

    [new] : Get all attempts of a user for a specific quiz + updated the quiz attempting functionality to identiy the inprogress attempts

--- src/main/java/com/hris/HRIS/controller/EmployeeQuizController.java
+++ src/main/java/com/hris/HRIS/controller/EmployeeQuizController.java
@@ -68,17 +68,33 @@ public class EmployeeQuizController {
                         Collections.shuffle(questionsList);
                     }
 
+                    Integer totalAttemptsTaken = 0;
+                    Boolean isAttemptInProgress = false;
+                    String existingInprogressAttemptId = "";
+
+                    for(EmployeeQuizModel existingAttempt : getAllQuizAttemptsById(requestBody)){
+                        if(existingAttempt.getStatus().equals("Inprogress")){
+                            isAttemptInProgress = true;
+                            existingInprogressAttemptId = existingAttempt.getId();
+                        }
+                        totalAttemptsTaken++;
+                    }
+
                     // Create a new attempt.
-                    EmployeeQuizModel employeeQuizModel = new EmployeeQuizModel();
+                    if(!isAttemptInProgress){
+                        EmployeeQuizModel employeeQuizModel = new EmployeeQuizModel();
 
-                    employeeQuizModel.setEmployeeEmail(employeeEmail);
-                    employeeQuizModel.setQuizId(quizId);
-                    // employeeQuizModel.setAttempt(1);
-                    employeeQuizModel.setAttemptDateTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
-                    employeeQuizModel.setSubmittedDateTime("0000-00-00 00:00:00");
-                    employeeQuizModel.setStatus("Inprogress");
+                        employeeQuizModel.setEmployeeEmail(employeeEmail);
+                        employeeQuizModel.setQuizId(quizId);
+                        // employeeQuizModel.setAttempt(1);
+                        employeeQuizModel.setAttemptDateTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")));
+                        employeeQuizModel.setSubmittedDateTime("0000-00-00 00:00:00");
+                        employeeQuizModel.setStatus("Inprogress");
 
-                    returnMsg = employeeQuizRepository.save(employeeQuizModel).getId();
+                        returnMsg = employeeQuizRepository.save(employeeQuizModel).getId();
+                    }else{
+                        returnMsg = existingInprogressAttemptId;
+                    }
                 }else{
                     returnMsg = "The requested user does not exists in the course";
                 }
@@ -93,6 +109,31 @@ public class EmployeeQuizController {
         result.add(questionsList);
 
         return result;
+    }
+
+    @GetMapping("/attempts/get/all")
+    public List<EmployeeQuizModel> getAllQuizAttemptsById(@RequestBody String requestBody){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        List<EmployeeQuizModel> attempsList = new ArrayList<>();
+        
+        try {
+            JsonNode requestBodyJson = objectMapper.readTree(requestBody);
+            String quizId = requestBodyJson.get("quizId").asText();
+            String employeeEmail = requestBodyJson.get("employeeEmail").asText();
+            
+
+            for(EmployeeQuizModel attempt : employeeQuizRepository.findAllByEmployeeEmail(employeeEmail)){
+                if (attempt.getQuizId().equals(quizId)){
+                    attempsList.add(attempt);
+                }
+            }
+
+        } catch (Exception e) {
+            // No action to take.
+        }
+
+        return attempsList;
     }
 
     @PostMapping("/submit")
