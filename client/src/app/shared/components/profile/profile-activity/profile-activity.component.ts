970cd01da7316d221057a9ed06ebae6034df878e
kavi castelo <kavindu123450@gmail.com>
2024-04-09 19:48:31 +0530

    profile activities
    implement profile activities window with test data

--- client/src/app/shared/components/profile/profile-activity/profile-activity.component.ts
+++ client/src/app/shared/components/profile/profile-activity/profile-activity.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {activityDataStore} from "../../../data-stores/activity-data-store";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-profile-activity',
   templateUrl: './profile-activity.component.html',
   styleUrls: ['./profile-activity.component.scss']
 })
-export class ProfileActivityComponent {
+export class ProfileActivityComponent implements OnInit{
+
+  constructor(private route: ActivatedRoute) {
+  }
+
+  activityDataStore: any = activityDataStore;
+
+  userId: any;
+  profileId:any;
+
+  ngOnInit(): void {
+    this.userId = localStorage.getItem('sender')
+    this.loadProfileId()
+    this.loadActivities()
+  }
+
+  loadProfileId(){
+    this.route.paramMap.subscribe(params =>{
+      this.profileId = params.get('id')
+    })
+  }
+
+  loadActivities(){
+    this.activityDataStore = this.activityDataStore.filter((feed:any) => (feed.userId == this.profileId) ? this.activityDataStore = [feed] : null )
+    this.activityDataStore.sort((a: any, b: any) => {
+      return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime(); // Reversed comparison logic
+    })
+  }
 
 }
---
+++ client/src/app/shared/data-stores/activity-data-store.ts
@@ -0,0 +1,121 @@
+import {ActivityModel} from "../data-models/Activity.model";
+
+export var activityDataStore: ActivityModel[] = [
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'66152846614a652719ca94ae',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'shared',
+        timestamp:'2024-04-07T08:44:39.157Z'
+    },
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'65d706364f1d870690fcb56c',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'liked',
+        timestamp:'2024-04-07T08:50:39.157Z'
+    },
+    {
+        userId:'66105c4922d9fd0f2042909f',
+        userName:'Mary',
+        postId:'66116cdf3329e9278c59f0ac',
+        posterName:'John',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'commented',
+        timestamp:'2024-04-07T07:44:39.157Z'
+    },
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'66116cdf3329e9278c59f0ac',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'commented',
+        timestamp:'2024-04-08T08:44:39.157Z'
+    },
+    {
+        userId:'66105c4922d9fd0f2042909f',
+        userName:'Mary',
+        postId:'66116cdf3329e9278c59f0ac',
+        posterName:'John',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'commented',
+        timestamp:'2024-04-06T08:44:39.157Z'
+    },
+    {
+        userId:'66105cd122d9fd0f204290a1',
+        userName:'Silvia',
+        postId:'66116cdf3329e9278c59f0ac',
+        posterName:'John',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'liked',
+        timestamp:'2024-03-07T08:44:39.157Z'
+    },
+    {
+        userId:'66105c4922d9fd0f2042909f',
+        userName:'Mary',
+        postId:'66125cf7feba733c47b4ceab',
+        posterName:'John',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'shared',
+        timestamp:'2024-04-07T08:10:39.157Z'
+    },
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'66125cf7feba733c47b4ceab',
+        posterName:'Mary',
+        postCaption:'',
+        action:'commented',
+        timestamp:'2024-04-07T01:44:39.157Z'
+    },
+    {
+        userId:'66105cd122d9fd0f204290a1',
+        userName:'Silvia',
+        postId:'661526b3614a652719ca94aa',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'commented',
+        timestamp:'2024-04-07T10:44:39.157Z'
+    },
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'661526b3614a652719ca94aa',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'liked',
+        timestamp:'2024-03-07T06:44:39.157Z'
+    },
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'661526b3614a652719ca94aa',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'liked',
+        timestamp:'2024-04-02T08:44:39.157Z'
+    },
+    {
+        userId:'66105b9c22d9fd0f2042909e',
+        userName:'John',
+        postId:'66152846614a652719ca94ae',
+        posterName:'Mary',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'shared',
+        timestamp:'2024-04-05T08:44:39.157Z'
+    },
+    {
+        userId:'66105c4922d9fd0f2042909f',
+        userName:'Mary',
+        postId:'661526b3614a652719ca94aa',
+        posterName:'John',
+        postCaption:'Wow, this article provided such valuable insights into the future',
+        action:'liked',
+        timestamp:'2024-04-07T08:49:39.157Z'
+    }
+]
