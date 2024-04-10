a49ff77957df8baf199041a9ab48890aa68917c4
kavi castelo <kavindu123450@gmail.com>
2024-04-10 21:04:31 +0530

    Fix common issues
    fix common visual issues and implements delete posts method

--- client/src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
+++ client/src/app/shared/dialogs/confirm-dialog/confirm-dialog.component.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {MultimediaService} from "../../../services/multimedia.service";
 
 @Component({
   selector: 'app-confirm-dialog',
@@ -10,7 +11,7 @@ export class ConfirmDialogComponent implements OnInit{
 
   receivedData:any;
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<ConfirmDialogComponent>) {
+  constructor(private multimediaService: MultimediaService, @Inject(MAT_DIALOG_DATA) public data: any, private ref: MatDialogRef<ConfirmDialogComponent>) {
   }
   ngOnInit(): void {
     this.receivedData = this.data
@@ -23,5 +24,13 @@ export class ConfirmDialogComponent implements OnInit{
   discardChanges(){
     this.receivedData.data.reset();
     this.closePopup();
+  }
+
+  deleteItem() {
+    this.multimediaService.deleteMultimedia(this.receivedData.data.id).subscribe(data => {
+      this.closePopup();
+    },error => {
+      console.log(error)
+    })
   }
 }
