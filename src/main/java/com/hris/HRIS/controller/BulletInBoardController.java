b6ff6aa584108e0086e7e550a6d291c35fd41b6b
kavi castelo <kavindu123450@gmail.com>
2024-04-24 14:01:40 +0530

    :[re] bulleting board
    upgraded bulleting board component params

--- src/main/java/com/hris/HRIS/controller/BulletInBoardController.java
+++ src/main/java/com/hris/HRIS/controller/BulletInBoardController.java
@@ -6,7 +6,9 @@ import com.hris.HRIS.repository.BulletInBoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,8 +19,28 @@ public class BulletInBoardController {
     BulletInBoardRepository bulletInBoardRepository;
 
     @PostMapping("/save")
-    public ResponseEntity<ApiResponse> saveBulletInBoard(@RequestBody BulletInBoardModel bulletInBoardModel){
-        bulletInBoardRepository.save(bulletInBoardModel);
+    public ResponseEntity<ApiResponse> saveBulletInBoard(@RequestParam("organizationId") String organizationId,
+                                                         @RequestParam("departmentId") String departmentId,
+                                                         @RequestParam("titleImage") MultipartFile titleImage,
+                                                         @RequestParam("title") String title,
+                                                         @RequestParam("message") String message,
+                                                         @RequestParam("redirectUrl") String redirectUrl,
+                                                         @RequestParam("action") String action,
+                                                         @RequestParam("backgroundImage") MultipartFile backgroundImage,
+                                                         @RequestParam("stringBg") String stringBg) throws IOException {
+
+        BulletInBoardModel newBulletinBoardModel = new BulletInBoardModel();
+        newBulletinBoardModel.setOrganizationId(organizationId);
+        newBulletinBoardModel.setDepartmentId(departmentId);
+        newBulletinBoardModel.setTitleImage(titleImage.getBytes());
+        newBulletinBoardModel.setTitle(title);
+        newBulletinBoardModel.setMessages(message);
+        newBulletinBoardModel.setRedirectUrl(redirectUrl);
+        newBulletinBoardModel.setAction(action);
+        newBulletinBoardModel.setBackgroundImage(backgroundImage.getBytes());
+        newBulletinBoardModel.setStringBg(stringBg);
+
+        bulletInBoardRepository.save(newBulletinBoardModel);
 
         ApiResponse apiResponse = new ApiResponse("Bullet in board saved successfully");
         return ResponseEntity.ok(apiResponse);
