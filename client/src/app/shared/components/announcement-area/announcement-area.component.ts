29b1d461acf7cda19df6b16f14958d6709d6fed9
kavi castelo <kavindu123450@gmail.com>
2024-04-27 20:27:34 +0530

    latest news
    client side

--- client/src/app/shared/components/announcement-area/announcement-area.component.ts
+++ client/src/app/shared/components/announcement-area/announcement-area.component.ts
@@ -5,6 +5,8 @@ import {ActivatedRoute} from "@angular/router";
 import {AuthService} from "../../../services/auth.service";
 import {Observable, tap} from "rxjs";
 import {SafeResourceUrl} from "@angular/platform-browser";
+import {LatestNewsService} from "../../../services/latest-news.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-announcement-area',
@@ -15,7 +17,19 @@ export class AnnouncementAreaComponent implements OnInit{
   bulletinsDataStore:any[] =[];
   filteredBulletins: any
 
+  newsDataStore:any[] = [];
+  filteredNews: any
+
+  maxNewsDisplayed: number = 3;
+  showAllNews: boolean = false;
+
+  newsForm = new FormGroup({
+    newsReUrl: new FormControl(null,[Validators.required]),
+    newsDes: new FormControl(null,[Validators.required])
+  })
+
   constructor(private bulletinService: BulletingBoardService,
+              private newsService: LatestNewsService,
               private multimediaService: MultimediaService,
               private route: ActivatedRoute,
               private cookieService: AuthService) {
@@ -24,6 +38,10 @@ export class AnnouncementAreaComponent implements OnInit{
   async ngOnInit(): Promise<any> {
     this.loadAllBulletins().subscribe(()=>{
       this.filterBulletins()
+    })
+
+    this.loadAllNews().subscribe(()=>{
+      this.filterNews()
     })
   }
 
@@ -63,5 +81,40 @@ export class AnnouncementAreaComponent implements OnInit{
       style['background-image'] = `url(data:image/jpeg;base64,${bulletin.backgroundImage})`;
     }
     return style;
+  }
+
+  loadAllNews(): Observable<any> {
+    return this.newsService.getAllNews().pipe(
+        tap(data => this.newsDataStore = data)
+    );
+  }
+
+  filterNews(): any[]{
+    const organization = this.cookieService.organization();
+    this.filteredNews = this.newsDataStore.filter((data:any) => data.organizationId == organization? this.filteredNews = [data]: this.filteredNews = null)
+    this.filteredNews.sort((a:any, b:any) => {
+      return new Date(b.timestamp).getTime() - new Date(a.timestamp).getTime()
+    })
+
+    return this.filteredNews;
+  }
+
+  toggleNews() {
+    this.showAllNews = !this.showAllNews;
+    if (this.showAllNews) {
+      this.maxNewsDisplayed = Infinity;
+    } else {
+      this.maxNewsDisplayed = 3;
+    }
+  }
+
+  openURL(url: string): void {
+    if (url) {
+      const anchor = document.createElement('a');
+      anchor.href = url;
+      anchor.target = '_blank';
+      anchor.rel = 'noopener noreferrer';
+      anchor.click();
+    }
   }
 }
