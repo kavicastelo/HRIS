a8a1e77f349bf25fd7ae685ac7a9d609ba28f773
PawaraLiyanage <158852235+PawaraLiyanage@users.noreply.github.com>
2024-05-04 14:26:26 -0700

    Update Attendance and Leave Model

--- src/main/java/com/hris/HRIS/service/LeaveService.java
+++ src/main/java/com/hris/HRIS/service/LeaveService.java
@@ -1,26 +1,43 @@
 package com.hris.HRIS.service;
 
+import com.hris.HRIS.model.AttendanceModel;
 import com.hris.HRIS.model.LeaveModel;
+import com.hris.HRIS.repository.LeaveRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 @Service
 public class LeaveService {
+
+    @Autowired
+    LeaveRepository leaveRepository;
     public LeaveModel createLeave(LeaveModel leaveModel) {
-        return null;
+        return leaveRepository.save(leaveModel);
     }
 
     public List<LeaveModel> getAllLeaves() {
-        return null;
-    }
-
-    public LeaveModel getLeaveById(String id) {
-        return null;
+        return leaveRepository.findAll();
     }
 
     public LeaveModel updateLeave(String id, LeaveModel leaveModel) {
-        return null;
+        LeaveModel existingLeaveModel = leaveRepository.findById(id).orElse(null);
+        if (existingLeaveModel != null) {
+            // Update the existing record with the new data
+            existingLeaveModel.setName(updatedAttendanceModel.getName());
+            existingAttendanceModel.setEmail(updatedAttendanceModel.getEmail());
+            // Update other fields as needed
+
+            // Save the updated record
+            return attendanceRepository.save(existingAttendanceModel);
+        } else {
+            return null; // Return null if the record with the given ID is not found
+        }
     }
+
+
+
+
 
     public boolean deleteLeave(String id) {
         return false;
