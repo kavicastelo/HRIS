cc866d2e4345a12aa9863248c69d154f0c923915
Raini Wijewantha <sashee.wijewantha@gmail.com>
2024-06-14 21:56:20 +0530

    Updated
    Co-Authored-By: kavi castelo <85213722+kavicastelo@users.noreply.github.com>

--- src/main/java/com/hris/HRIS/repository/ApplyJobRepository.java
+++ src/main/java/com/hris/HRIS/repository/ApplyJobRepository.java
@@ -4,4 +4,5 @@ import com.hris.HRIS.model.ApplyJobModel;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface ApplyJobRepository extends MongoRepository<ApplyJobModel, String> {
+
 }
