b9e0c00f1f8960a407bc7f77f387d81306d313d1
Raini Wijewantha <sashee.wijewantha@gmail.com>
2024-06-20 23:54:52 +0530

    Update Added
    Co-Authored-By: kavi castelo <85213722+kavicastelo@users.noreply.github.com>

--- src/main/java/com/hris/HRIS/dto/MeetingRequest.java
+++ src/main/java/com/hris/HRIS/dto/MeetingRequest.java
@@ -3,19 +3,22 @@ package com.hris.HRIS.dto;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.sql.Time;
-import java.text.DateFormat;
-import java.util.Date;
 
 @Getter
 @Setter
 public class MeetingRequest {
 
     private String id;
-    private Date meetingDate;
-    private Date meetingTime;
+
+    private String meetingDate;
+
+    private String meetingTime;
+
     private String meetingLink;
+
     private String userEmail;
+
     private String managerEmail;
+
     private boolean notify;
 }
--- src/main/java/com/hris/HRIS/model/ApplyJobModel.java
+++ src/main/java/com/hris/HRIS/model/ApplyJobModel.java
@@ -5,12 +5,7 @@ import lombok.Setter;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.sql.Time;
-import java.text.DateFormat;
-import java.util.Date;
 
 @Getter
 @Setter
@@ -26,7 +21,7 @@ public class ApplyJobModel {
 
     private String job_title;
 
-    private Date apply_date;
+    private String apply_date;
 
     private String contact_number;
 
@@ -34,15 +29,15 @@ public class ApplyJobModel {
 
     private byte[] cv;
 
-    private boolean action = false; //1st stage to 2nd stage
+    private boolean action; //1st stage to 2nd stage
 
-    private boolean hire = false; //2nd stage to hiring stage
+    private boolean hire; //2nd stage to hiring stage
 
     private String meeting_link;
 
-    private Date meeting_date;
+    private String meeting_date;
 
-    private Date meeting_time;
+    private String meeting_time;
 
     private boolean notify;
 
